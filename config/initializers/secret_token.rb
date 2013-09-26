79f96e1043cd88d0d5f13335e842e6899353d7d5
Brian Collins <brian.pyrrho@bigpond.com>
2013-09-26 16:53:46 +1000

    rails new pops

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pops::Application.config.secret_key_base = 'b6e12076ef74cfb845eb60740145d74703df8a59fc2d2e8f762eca226e363ddee0f8859fdc27758f3c9f08de4a990d055e09719c575a0e6b7e16a34aa4d61954'
