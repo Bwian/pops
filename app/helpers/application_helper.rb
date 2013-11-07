28397dcdf937706495f1c143eeed05ca63a20ea4
Brian Collins <brian.pyrrho@bigpond.com>
2013-11-07 18:35:36 +1100

    Added button to return to Users screen

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,20 +2,11 @@ module ApplicationHelper
   
   LINK_STYLE = "btn btn-primary btn-sm"
   
-  # This routine returns html links to be used to show, edit or delete data from a model.
-  # It relies on the class being passed in conforming to the rails model naming conventions
-  def show_edit_destroy(model)
+  def link_list(model)
     name = model.class.name.downcase
-    actions = link_to('Show', model)
-    if session[:admin]
-      actions << ' | '
-      actions << link_edit(model)
-      actions << ' | '
-      actions << link_to('Destroy', model, method: :delete, data: { confirm: 'Are you sure?'})
-    end
-    actions
+    link_to('List', "/#{name.pluralize}", class: LINK_STYLE)
   end
-
+  
   def link_new(name)
     session[:admin] ? link_to("New #{name.titleize}", "/#{name.pluralize}/new", class: LINK_STYLE) : ""
   end
@@ -29,6 +20,11 @@ module ApplicationHelper
     session[:admin] ? link_to('Delete', model, method: :delete, data: { confirm: 'Are you sure?' }, class: "btn btn-danger btn-sm" ) : ""
   end
   
+  def link_logoutin
+    return '' if request.fullpath == '/login'
+    session[:user_id] ? link_to('Logout','/logout') : link_to('Login', '/login')
+  end
+    
   def index_header(name,columns)
     link = session[:admin] ? link_to("New #{name.titleize}", "/#{name.pluralize}/new", class: LINK_STYLE) : ""
     header = "<tr><td colspan=\"#{columns - 1}\" class=\"header\">#{name.pluralize.titleize}</td><td align=\"right\" class=\"header\">#{link}</td></tr><tr><td></td></tr>"
@@ -39,10 +35,6 @@ module ApplicationHelper
     disabled ? name : "Enter #{name}"
   end
 
-  def log_link
-    return '' if request.fullpath == '/login'
-    session[:user_id] ? link_to('Logout','/logout') : link_to('Login', '/login')
-  end
 end
 
 
