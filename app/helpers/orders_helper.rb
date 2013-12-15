b1544145a5c0b6357aefad1989da6d285436b818
Brian Collins <brian.pyrrho@bigpond.com>
2013-12-15 22:52:20 +1100

    Pass arrays to view for atby and order_action

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -2,4 +2,44 @@ module OrdersHelper
   def link_item_new(order)
     session[:admin] ? link_to("Add Item", "/orders/#{order.id}/items/new", class: ApplicationHelper::LINK_STYLE) : ""
   end
+
+  def link_action(order)  
+    actions = []
+    case order.status
+      when OrderStatus::DRAFT
+        actions = ['submit'] if order.items.any?
+      when OrderStatus::SUBMITTED
+        actions = ['draft','approve']
+      when OrderStatus::APPROVED
+        actions = ['submit','complete']
+    end
+    
+    links = []
+    actions.each do |action|
+      link = session[:admin] ? link_to(action_label(action), "/orders/#{order.id}/#{action}", method: :post, class: ApplicationHelper::LINK_STYLE) : ""
+      links << link
+    end
+    
+    links
+  end
+  
+  def order_actions(order,readonly)
+    actions = []
+    if readonly
+      actions.concat(link_action(order))
+      actions << link_item_new(order)
+      actions << link_edit(order)
+      actions << link_delete(order)
+    else  
+      submit_label = order.id ? 'Update Order' : 'Create Order'
+      actions << submit_tag(submit_label, class: ApplicationHelper::LINK_STYLE)
+    end
+    actions << link_list(order)
+  end
+  
+  private
+  
+  def action_label(action)
+    action == 'complete' ? 'Process' : action.capitalize
+  end
 end
