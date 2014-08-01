1f85b208f74673c66994965c0c273afb2490e48a
Brian Collins <brian.pyrrho@bigpond.com>
2014-08-01 17:31:02 +1000

    Added SQL Server insert statements - best guess

--- app/models/exo_agent.rb
+++ app/models/exo_agent.rb
@@ -2,6 +2,8 @@ require 'tiny_tds'
 
 class ExoAgent
   
+  NIL = 'nil'
+  
   attr_reader :notice
   attr_writer :host, :port, :user, :database
   
@@ -53,6 +55,41 @@ class ExoAgent
   def complete(order)
     return false unless connect
     
+    invoice_id = insert('ACS_Create_CrInv_Hdr_Record',
+      order.supplier_id,
+      DateTime.now, # or order.invoice_date ???
+      order.invoice_no || NIL,
+      order.reference,
+      NIL, # REF2
+      order.subtotal,
+      order.gst, # or grandtotal ???
+      0.0, # MANUAL_ROUNDING
+      NIL, # SALESNO
+      order.payment_date || NIL,
+      NIL, # X_SIMPRO_ID
+      NIL, # X_SIMPRO_COMPANY
+      NIL  # BRANCHNO
+    )
+
+    return false if !invoice_id
+    
+    @order.items.each do |item|
+      return false if !insert('ACS_Create_CrInv_GLLine_Record',
+      order.supplier_id,
+      order.invoice_no,
+      invoice_id,
+      item.program_id,
+      item.account_id,
+      NIL, # GLSUBACC
+      item.description,
+      1.0, # QUANTITY
+      item.price,
+      item.tax_rate_id,
+      DateTime.now, # or something else ???
+      NIL # LINKED_STOCKCODE
+      )
+    end
+    
     @connection.close
     return true
   end
@@ -68,6 +105,19 @@ class ExoAgent
     "select #{s.strip.gsub(' ',',')} from [#{@names[table_name]}]"
   end
 
+  
+  def valid?
+    [:host,:port,:user,:password,:database].each do |vbl|
+      unless self.instance_variable_get("@#{vbl.to_s}")
+        @notice = "SQL Server configuration invalid - missing #{vbl.to_s} setting"
+        return false
+      end
+    end
+    
+    @login_timeout = 1 if !@login_timeout || @login_timeout == 0
+    return true
+  end 
+   
   def connect
     return false unless valid?
     
@@ -87,16 +137,22 @@ class ExoAgent
     return true
   end
   
-  def valid?
-    [:host,:port,:user,:password,:database].each do |vbl|
-      unless self.instance_variable_get("@#{vbl.to_s}")
-        @notice = "SQL Server configuration invalid - missing #{vbl.to_s} setting"
-        return false
+  def insert(proc,args)
+    exec = "[#{proc}] #{args.join(' ')}"
+    stmt = "declare @SeqNo int; exec #{exec} ; select @SeqNo as seqno;"
+    seqno = nil
+    begin
+      result = client.execute(stmt)
+      result.each(:symbolize_keys => true) do |row|
+        seqno = row[:seqno]
       end
+    rescue TinyTds::Error
+      @notice = "Error processing: #{exec}"
+      return nil
     end
+    @notice = "Insert failed: #{exec}" unless seqno
     
-    @login_timeout = 1 if !@login_timeout || @login_timeout == 0
-    return true
-  end 
-   
+    return seqno
+  end
+  
 end
