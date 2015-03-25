00c29f21231a2b5ea8f9b483a83bca9ea32b9561
Brian Collins <brian.pyrrho@bigpond.com>
2015-03-25 20:42:30 +1100

    Use configuration files to determin host for links in email messages.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,8 @@ Pops::Application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
+  
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -26,4 +27,5 @@ Pops::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  
 end
