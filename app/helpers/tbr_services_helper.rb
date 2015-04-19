3b3634bf9d9ec0c0c5e16ee4ef42aa75586fbd41
Brian Collins <brian.pyrrho@bigpond.com>
2015-04-19 23:52:22 +1000

    Format phone numbers

--- app/helpers/tbr_services_helper.rb
+++ app/helpers/tbr_services_helper.rb
@@ -1,2 +1,5 @@
 module TbrServicesHelper
+  def format_phone_number(code)
+    code[0] == '0' ? "#{code[0,2]}&nbsp;#{code[2,4]}&nbsp;#{code[6,10]}".html_safe : code
+  end
 end
--- test/helpers/tbr_services_helper_test.rb
+++ test/helpers/tbr_services_helper_test.rb
@@ -1,4 +1,8 @@
 require 'test_helper'
 
 class TbrServicesHelperTest < ActionView::TestCase
+  test "format_phone_number" do
+    assert_equal('04&nbsp;1850&nbsp;5555',format_phone_number('0418505555'))
+    assert_equal('N1234',format_phone_number('N1234'))
+  end
 end
