e90cdf12c0bc89a08dc097fbc0e58d0f64deae42
Brian Collins <brian.pyrrho@bigpond.com>
2015-06-02 23:10:30 +1000

    Style selects on Exo maintenance screens

--- app/helpers/exo_helper.rb
+++ app/helpers/exo_helper.rb
@@ -6,6 +6,20 @@ module ExoHelper
     authorised_action(EDIT,params[:controller], model) ? link_to(model.id, url_for(controller: name, id: model.id, action: :edit), class: 'link_exo') : model.id
   end
   
+  # These exo_ methods could probably be DRYed
+  
+  def exo_status(model)
+    "'##{model.class.name.underscore}_status'".html_safe
+  end
+  
+  def exo_tax_rate(model)
+    "'##{model.class.name.underscore}_tax_rate_id'".html_safe
+  end
+  
+  def exo_payment_term(model)
+    "'##{model.class.name.underscore}_payment_term_id'".html_safe
+  end
+  
   def attribute_label(field)
     label = label_tag field
     label.sub('</',':</').html_safe
@@ -15,7 +29,8 @@ module ExoHelper
     if field =~ /_id$/
       class_name = field.sub('_id','').camelize
       klass = Object.const_get(class_name)
-      f.select field, klass.selection, { include_blank: true }, { class: "btn btn-primary btn-sm" }
+      select = f.select field, klass.selection, { include_blank: true }, {}
+      "<div class='select-wide'> #{select} </div>".html_safe
     else
       f.text_field(field)
     end
--- test/helpers/exo_helper_test.rb
+++ test/helpers/exo_helper_test.rb
@@ -5,6 +5,7 @@ class ExoHelperTest < ActionView::TestCase
   setup do
     setup_admin_session
     @account = accounts(:one)
+    @tax_rate = tax_rates(:none)
     params[:controller] = 'accounts'
   end
   
@@ -16,6 +17,11 @@ class ExoHelperTest < ActionView::TestCase
   test "link_exo - creator" do
     setup_creator_session
     assert('',link_exo(@account))
+  end
+  
+  test "exo_status" do
+    assert_equal("'#account_status'",exo_status(@account))
+    assert_equal("'#tax_rate_status'",exo_status(@tax_rate),'CamelCase model') 
   end
   
   test "attribute_label" do
@@ -31,7 +37,7 @@ class ExoHelperTest < ActionView::TestCase
   
   test "attribute_field - select" do
     form_for(@account) do |f|
-      assert(attribute_field(f,'tax_rate_id').starts_with?('<select'))
+      assert(attribute_field(f,'tax_rate_id').starts_with?('<div'))
       assert(/account\[tax_rate_id\]/ =~ attribute_field(f,'tax_rate_id'))
     end
   end
