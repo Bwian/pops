659c14ae32ce37f7a82e5d0e7d93b027d8c053cb
Brian Collins <brian.pyrrho@bigpond.com>
2015-08-09 21:33:40 +1000

    Test for reminder emails

---
+++ db/utilities/mailtest.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+require 'pry'
+require "#{File.dirname(__FILE__)}/../../config/environment"
+
+user = User.new
+user.code = 'test'
+user.name = 'Test Name'
+user.email = 'brian.pyrrho@bigpond.com'
+
+summary_list = ['Line 1','Line 2']
+mail = OrderMailer.reminder_summary(user,summary_list)
+mail.deliver
+
+Rails.logger.info("Reminder mail test")
