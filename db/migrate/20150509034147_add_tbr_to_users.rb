6d9311c8058dceadd35800b7c75ca1fbe5d1b379
Brian Collins <brian.pyrrho@bigpond.com>
2015-08-20 22:50:19 +1000

    Fix defaults for tbr_admin and tbr_manager

--- db/migrate/20150509034147_add_tbr_to_users.rb
+++ db/migrate/20150509034147_add_tbr_to_users.rb
@@ -1,6 +1,6 @@
 class AddTbrToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :tbr_admin, :boolean
-    add_column :users, :tbr_manager, :boolean
+    add_column :users, :tbr_admin, :boolean, default: false
+    add_column :users, :tbr_manager, :boolean, default: false
   end
 end
